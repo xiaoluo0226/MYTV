14b7850684f5b2be148805797ec678f42ac0c9d1
xiaoluo0226 <154872235+xiaoluo0226@users.noreply.github.com>
2024-03-29 13:39:06 +0800

    SearchHD.py

--- temp/SearchHD.py
+++ temp/SearchHD.py
@@ -323,12 +323,12 @@ results.sort(key=lambda x: (x[0], -float(x[2].split()[0])))
 results.sort(key=lambda x: channel_key(x[0]))
 
 # 将结果写入文件
-with open("itv_results.txt", 'w', encoding='utf-8') as file:
+with open("itvHD_results.txt", 'w', encoding='utf-8') as file:
     for result in results:
         channel_name, channel_url, speed = result
         file.write(f"{channel_name},{channel_url},{speed}\n")
 
-with open("itv_speed.txt", 'w', encoding='utf-8') as file:
+with open("itvHD_speed.txt", 'w', encoding='utf-8') as file:
     for result in results:
         channel_name, channel_url, speed = result
         file.write(f"{channel_name},{channel_url}\n")
